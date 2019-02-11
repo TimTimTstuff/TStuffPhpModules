376251c79cad3652aa8ccc586d1a42225b5aacc5
TimTimTstuff <timo.kesselring@tstuff.de>
2019-02-11 15:21:15 +0100

    update tests

--- examples/db.php
+++ examples/db.php
@@ -1,6 +1,7 @@
 <?php
 
 use TStuff\Php\DBMapper\TDBMapper;
+use TestClass\DbUser;
 session_start();
 define("BASE_PATH", $_SERVER['CONTEXT_DOCUMENT_ROOT'] . "/TStuffPhpModules/");
 define("DB_DSN", "mysql:host=localhost;dbname=tstuff_test;charset=utf8mb4");
@@ -15,7 +16,7 @@ try{
        DB_PASS,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+           // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false
         ]//options
     );
@@ -29,5 +30,4 @@ try{
 $mapper = new TDBMapper($pdo);
 
 $mapper->registerObject("TestClass\DbUser");
-
-$mapper->createDatabase();
+$mapper->updateDatabase();
