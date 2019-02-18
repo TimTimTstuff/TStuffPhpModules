764f439a0f2072fb87f104c87c57a2d79ec8f4a6
TimTimTstuff <timo.kesselring@tstuff.de>
2019-02-18 19:20:11 +0100

    add tests

--- examples/dbmapper/querys.php
+++ examples/dbmapper/querys.php
@@ -35,14 +35,47 @@ $y->lastLogin = time();
 $y->password = "massword";
 $y->isAdmin = true;
 
+
 //DbUser::create($y);
+
+$p = new DbProducts();
+$p->description = "Product 1";
+$p->inStock = 5;
+$p->ownerUserId = 1;
+$p->price = 5.52;
+$p->soldAmount = 0;
+$p->tax = 19;
+$p->name = "My first Product";
+$p2 = new DbProducts();
+$p2->description = "Product 2";
+$p2->inStock = 3;
+$p2->ownerUserId = 2;
+$p2->price = 5.55;
+$p2->soldAmount = 5;
+$p2->tax = 7;
+$p2->name = "My second Product";
+
+DbProducts::createAll([$p,$p2]);
+
+DbProducts::deleteBy("1=1 limit 1, 10 ");
+
+$products = DbProducts::all("1 = 1");
+
+foreach($products as $v){
+    echo $v->name." <br/>";
+}
+
+//DbProducts::delete($products);
+
 /**
  * @var DbUser[] $myUsers
  */
-$myUsers = DbUser::all(" name like '%timo%'");
+$myUsers = DbUser::all(" 1=1 ");
 
 foreach($myUsers as $k => $u){
     echo $u->name." <br/>";
-    $u->name = $u->name."_".$k;
-    $u->save();
+    
+
 }
+
+DbUser::delete(DbUser::all("1=1 limit 1, 100"));
