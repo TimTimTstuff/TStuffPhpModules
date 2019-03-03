0b733a5ce6deccb02b99c6713ccf63b6ba1b7e68
TimTimTstuff <timo.kesselring@tstuff.de>
2019-03-03 11:52:19 +0100

    Create Logger and Default Logger (Echo, File, Db)

--- TStuff/Php/Logging/LogLevel.php
+++ TStuff/Php/Logging/LogLevel.php
@@ -2,11 +2,11 @@
 namespace TStuff\Php\Logging  ; 
 
     abstract class LogLevel  {
-        const Trace =   0b00000001;
-        const Debug =   0b00000010;
-        const Info =    0b00000100;
-        const Warning = 0b00001000;
-        const Error =   0b00010000;
-        const Fatal =   0b00100000;
+        const Trace =   0;
+        const Debug =   1;
+        const Info =    2;
+        const Warning = 4;
+        const Error =   8;
+        const Fatal =   16;
        
     }
